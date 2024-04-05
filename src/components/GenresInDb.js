05bcabf49a36d53ac5fb1b475c1805c85365bb72
AdrianAubel <adrian.aubel@gmail.com>
2024-04-04 21:48:46 -0300

    iniciando dashboard

---
+++ src/components/GenresInDb.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function GenresInDb() {
+  return (
+    <div className="col-lg-6 mb-4">
+      <div className="card shadow mb-4">
+        <div className="card-header py-3">
+          <h5 className="m-0 font-weight-bold text-gray-800">
+            Products
+          </h5>
+        </div>
+        <div className="card-body">
+          <div className="row">
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+                <div className="card-body">Hombre</div>
+              </div>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+                <div className="card-body">Mujer</div>
+              </div>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+                <div className="card-body">Kids</div>
+              </div>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+                <div className="card-body">Sale</div>
+              </div>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+                <div className="card-body">All</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GenresInDb;
