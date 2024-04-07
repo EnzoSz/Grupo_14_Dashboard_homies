ecb0f39a5e011c17e7f0d827b9000ae3e50ed251
AdrianAubel <adrian.aubel@gmail.com>
2024-04-07 13:35:33 -0300

    fetch users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,44 +1,63 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import SmallCard from './SmallCard';
 
-/*  Cada set de datos es un objeto literal */
-
-/* <!-- Products in DB --> */
-
-let productsInDB = {
-    title: 'Products in Data Base',
-    color: 'primary', 
-    cuantity: 21,
-    icon: 'fa-clipboard-list'
-}
-
-/* <!-- Total awards --> */
-
-let totalAwards = {
-    title:' Total awards', 
-    color:'success', 
-    cuantity: '79',
-    icon:'fa-award'
-}
-
-/* <!-- Actors quantity --> */
-
-let actorsQuantity = {
-    title:'Actors quantity' ,
-    color:'warning',
-    cuantity:'49',
-    icon:'fa-user-check'
-}
-
-let cartProps = [productsInDB, totalAwards, actorsQuantity];
 
 function Users(){
+
+    const [ usersInDb, setUsersInDb ] = useState([]);
+    const [ productsInDb, setProductsInDb ] = useState([]);
+
+    useEffect(() => {
+      fetch('http://localhost:3000/api-user')
+      .then(response => response.json())
+      .then(data => { setUsersInDb(data)
+        console.log(data);
+    })
+      .catch(error => console.log(error));
+    }, []);
+
+
+    useEffect(() => {
+      fetch('http://localhost:3000/api-products')
+      .then(response => response.json())
+      .then(data => { setProductsInDb(data) })
+      .catch(error => console.log(error));
+    }, []);
+
+    /* <!-- products --> */
+
+    let products = {
+        title: 'Productos',
+        color: 'primary', 
+        cuantity: productsInDb.length,
+        icon: 'fa-clipboard-list'
+    }
+    
+    /* <!-- category --> */
+    
+    let category = {
+        title:'Categorias', 
+        color:'success', 
+        cuantity: "5",
+        icon:'fa-award'
+    }
+    
+    /* <!-- usuarios --> */
+    
+    let users = {
+        title: "Usuarios" ,
+        color:'warning',
+        cuantity: usersInDb.length,
+        icon:'fa-user-check'
+    }
+
+    let cartProps = [products, category, users];
+
     return (
     
         <div className="row">
             
             {cartProps.map( (product, i) => {
-
                 return <SmallCard {...product} key={i}/>
             
             })}
